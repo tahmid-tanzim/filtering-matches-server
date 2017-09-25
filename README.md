184c8d1ce20fb3429ddf4395f7c050beb6c2c009
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2017-09-25 21:03:40 +0600

    has_photo and in_contact filter added

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 1. Clone this repo from `https://github.com/tahmid-tanzim/filtering-matches-server.git`
 2. Run `npm install` from the root directory
-3. Run `npm start`
+3. Run `npm run dev`
 
-Your API will now be served to `localhost:8000` by default.
+Your API will now be served to `localhost:8080` by default.
 
-Sample API
+Sample API Endpoints
 ```
-http://localhost:8080/api/v1/person?hello=world&foo=90
+http://localhost:8080/api/v1/person?age=18&age=95&compatibility_score=1&compatibility_score=99&height=135&height=210 
 
 ```
+
+**Note:** I haven't implement any data storage for this coding exercise. Simply export object from `api/models/matches.js`.
--- api/controllers/persons.js
+++ api/controllers/persons.js
@@ -24,25 +24,43 @@ exports.get = function (req, res) {
     };
 
     var filteredData = matches.matches
-        //.filter(function (item) {
-        //    return query.hasOwnProperty('has_photo') ? item['has_photo'].toString() === query['has_photo'] : true;
-        //})
-        .filter(function (item) {
-            return query.hasOwnProperty('favourite') ? item.favourite.toString() === query.favourite : true;
-        })
-        .filter(function (item) {
-            return item.age >= parseInt(query.age[0]) && item.age <= parseInt(query.age[1]);
-        })
-        .filter(function (item) {
-            return item.height_in_cm >= parseInt(query.height[0]) && item.height_in_cm <= parseInt(query.height[1]);
-        })
-        .filter(function (item) {
-            var score = item.compatibility_score * 100;
-            return score >= parseInt(query.compatibility_score[0]) && score <= parseInt(query.compatibility_score[1]);
-        })
-        .filter(function (item) {
-            return distance.getDistanceFromLatLonInKm(item.city.lat, item.city.lon, myCity.lat, myCity.lon) >= parseInt(query.distance_in_km);
-        });
+            .filter(function (item) {
+                if (!query.hasOwnProperty('has_photo')) {
+                    return true;
+                }
+
+                return (query['has_photo'] === 'true' && item.hasOwnProperty('main_photo')) || (query['has_photo'] === 'false' && !item.hasOwnProperty('main_photo'))
+            })
+            .filter(function (item) {
+                if (!query.hasOwnProperty('in_contact')) {
+                    return true;
+                }
+
+                return (query['in_contact'] === 'true' && item.contacts_exchanged > 0) || (query['in_contact'] === 'false' && item.contacts_exchanged === 0)
+            })
+            .filter(function (item) {
+                return query.hasOwnProperty('favourite') ? item.favourite.toString() === query.favourite : true;
+            })
+            .filter(function (item) {
+                return item.age >= parseInt(query.age[0]) && item.age <= parseInt(query.age[1]);
+            })
+            .filter(function (item) {
+                return item.height_in_cm >= parseInt(query.height[0]) && item.height_in_cm <= parseInt(query.height[1]);
+            })
+            .filter(function (item) {
+                var score = item.compatibility_score * 100;
+                return score >= parseInt(query.compatibility_score[0]) && score <= parseInt(query.compatibility_score[1]);
+            })
+            .filter(function (item) {
+                if (!query.hasOwnProperty('distance_in_km')) {
+                    return true;
+                }
+
+                var diff = distance.getDistanceFromLatLonInKm(item.city.lat, item.city.lon, myCity.lat, myCity.lon);
+                var index = parseInt(query.distance_in_km);
+                return (index === 0 && diff < 30) || (index === 1 && diff >= 30 && diff <= 300) || (index === 2 && diff > 300);
+            })
+        ;
 
     return res.status(200).json({matches: filteredData});
 };
